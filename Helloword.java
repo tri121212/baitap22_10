06e2c3001cbd6612e04d5e707316a264720b4794
tri121212 <tri.16102006@gmail.com>
2025-09-03 16:03:25 +0700

    ok

--- Helloword.java
+++ Helloword.java
@@ -2,8 +2,8 @@ public class Helloword {
     public static void main(String[] args) {
         person a = new person();
         a.name = "Chau_Kter";
-        a.height = 1.7f;
-        a.age = 21;
+        a.height = 1.8f;
+        a.age = 22;
         System.out.println(a.name);
         System.out.println(a.height);
         System.out.println(a.age);

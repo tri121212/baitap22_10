0aeb53c8b1616bb169b461625509b56d2faf1cdb
tri121212 <tri.16102006@gmail.com>
2025-09-03 15:48:33 +0700

    ok

---
+++ Helloword.java
@@ -0,0 +1,11 @@
+public class Helloword {
+    public static void main(String[] args) {
+        person a = new person();
+        a.name = "Chau_Kter";
+        a.height = 1.7f;
+        a.age = 21;
+        System.out.println(a.name);
+        System.out.println(a.height);
+        System.out.println(a.age);
+    }
+}
